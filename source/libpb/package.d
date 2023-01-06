2f5710fafb06bbccfee095953f10e12d1eb2e3bf
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-01-06 16:38:03 +0200

    - Removed uneeded public imports

--- source/libpb/package.d
+++ source/libpb/package.d
@@ -2,7 +2,3 @@ module libpb;
 
 public import libpb.exceptions;
 public import libpb.driver;
-
-// These being brought in means they can UDT (user-defined types) like enums it cannot see otherwise
-public import libpb.serialization : serializeRecord;
-public import libpb.deserialization : fromJSON;
